25e97490710a30ee16ea243c6d0931b3387301be
Barroqueiro <cruzdinis@ua.pt>
2022-05-09 14:31:01 +0100

    Fixed

--- VS/InstallAndRunHorusec.sh
+++ VS/InstallAndRunHorusec.sh
@@ -4,11 +4,9 @@ assets="$1"
 
 echo assets
 
-mv $2 .
-
 # Install and run horusec
 curl -fsSL https://raw.githubusercontent.com/ZupIT/horusec/main/deployments/scripts/install.sh | bash -s latest
-horusec start -p="./" -e="true" -o="json" -O="./full_report.json" $3
+horusec start -p="./" -e="true" -o="json" -O="./full_report.json" --config-file-path $2 $3
 ret=$?
 
 # Sumarise reports
