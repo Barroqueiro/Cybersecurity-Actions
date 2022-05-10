ccc40048586d3e2ad7f144db1f846835bf77462e
Barroqueiro <cruzdinis@ua.pt>
2022-05-10 15:45:16 +0100

    Trivy fail on vulns

--- TS/InstallAndRunTrivy.sh
+++ TS/InstallAndRunTrivy.sh
@@ -18,10 +18,13 @@ mv $2 .
 curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sh -s -- -b /usr/local/bin v0.18.3
 
 # Run trivy against the image to scan
-trivy image $3 --format json -o TrivyReport.json $4
+trivy image $3 --exit-code 1 --format json -o TrivyReport.json $4
+ret=$?
 
 # Sumarize the reports
 mkdir -p $dir
 python3 -m pip install Jinja2
 python3 $assets/TrivyReporting.py TrivyReport.json $assets> $dir/TrivyReport.html
 mv TrivyReport.json  $dir
+
+exit $ret
